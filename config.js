48eed3027100f88b6e6911163975478fc233f41f
Edoardo Lucini <assassineeeto@gmail.com>
2022-06-05 18:42:31 +0200

    added comment

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     WALLET: [process.env.WALLET], // insert wallets's addresses able to play the game (substitute process.env.WALLET with your wallets)
     PVT_KEY: [process.env.PVT_KEY], // same as above with pvt_key; put wallet's correspondent pvt_key in the same order
     SERVER_ENDPOINT: process.env.SERVER_ENDPOINT, // use the server endpoint you prefer
-    WAITING_TIME_MS: process.env.WAITING_TIME_MS,
+    WAITING_TIME_MS: process.env.WAITING_TIME_MS, // waiting time for the next thing brawl in ms; should be greater than 1 hour
 };
 
 module.exports = { config };
